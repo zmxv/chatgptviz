5a6baf589ab935366654838f96baca37dc56805e
Zhen Wang <z@zmxv.com>
2025-01-15 20:37:10 +0800

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+//@ts-nocheck
+import CalHeatmap from 'cal-heatmap';
+import 'cal-heatmap/cal-heatmap.css';
+import './style.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const cal: CalHeatmap = new CalHeatmap();
+  const fileInput = document.getElementById('fileInput') as HTMLInputElement;  
+  fileInput.addEventListener('change', async () => {
+    const file = fileInput.files?.[0];
+    if (file) {
+      const text = await file.text();
+      const conversations = JSON.parse(text);
+      cal.paint({
+        itemSelector: '#cal',
+        range: new Date().getFullYear() - 2023 + 1,
+        domain: { type: 'year' },
+        subDomain: { type: 'day' },
+        date: { start: new Date(2023, 1, 1) },
+        verticalOrientation: true,
+        animationDuration: 0,
+        theme: 'dark',
+        data: {
+          source: conversations,
+          type: 'json',
+          x: d => d['create_time'] * 1000,
+          y: () => 1,
+        },
+        scale: {
+          color: {
+            domain: [0, 10],
+          }
+        }
+      });
+    }
+  });
+
+  const fileButton = document.getElementById('fileButton')!;
+  fileButton.addEventListener('click', () => fileInput.click());
+});
